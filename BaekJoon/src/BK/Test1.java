24b161fd41bd10b230edf414d2f792b9409feb37
byunjaejin <byunjaejin@192.168.7.2>
2022-04-12 23:01:10 +0900

    테스트

---
+++ BaekJoon/src/BK/Test1.java
@@ -0,0 +1,10 @@
+package BK;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("커밋/푸시 테스트2");
+	}
+
+}
--- BaekJoon/src/BK/test2.java
+++ BaekJoon/src/BK/test2.java
@@ -1,6 +1,6 @@
 package BK;
 
-public class Main {
+public class test2 {
 	public static void main(String[] args) {
 		System.out.println("커밋 / 푸시 테스트");
 	}
