c9bcf923308fd59ea7fb4bed00dc5ba783e28238
JaeJinByun <byunjin11@naver.com>
2022-04-25 16:12:41 +0000

    23037 solve 제곱근 연산

---
+++ BaekJoon/src/B5_23037/Main.java
@@ -0,0 +1,20 @@
+package B5_23037;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new  Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int result = 0;
+		for(int i=0; i<5; i++) {
+			int b = a%10;
+			a /= 10;
+			result += Math.pow(b, 5);
+		}
+		System.out.println(result);
+	}
+
+}
