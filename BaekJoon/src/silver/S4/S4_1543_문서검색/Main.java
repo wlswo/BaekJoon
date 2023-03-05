32dbc1f79884154802a19e4d1a1e339dc5bd0712
JaeJinByun <byunjin11@naver.com>
2023-03-05 23:16:38 +0900

    S4 [문서 검색] 문자열 , 풀이 추가

--- BaekJoon/src/silver/S4/S4_1543_문서검색/Main.java
+++ BaekJoon/src/silver/S4/S4_1543_문서검색/Main.java
@@ -24,11 +24,48 @@ public class Main {
         target = target.replace(temp,"");
 
         System.out.println( (size1-target.length())/size2 );
+
+
+    }
+
+    /**
+     * Solution1
+     * abcababcabc : abc
+     * (abc)ab(abc)(abc) -> ab
+     * 원래 길이 (11) -> replace 후 길이 (2) = 9개의 길이문자가 바뀜
+     * 이는 즉 9개의 길이를 temp 로 나누면 바꾼 단어의 개수를 알 수 있다.
+     */
+    public void Solution1(String target, String temp) {
+        int size1 = target.length();
+        int size2 = temp.length();
+
+        target = target.replace(temp,"");
+
+        System.out.println( (size1-target.length())/size2 );
+    }
+
+    /**
+     * Solution2
+     * abcababcabc : abc
+     * 길이 하나하나 체크
+     */
+    public void Solution2(String target, String temp) {
+        int size1 = target.length();
+        int size2 = temp.length();
+        int start = 0;
+        int end = size2;
+        int answer = 0;
+        while (end <= size1){
+            if (target.substring(start, end).equals(temp)) {
+                answer++;
+                start += size2;
+                end += size2;
+            } else {
+                start++;
+                end++;
+            }
+        }
+        System.out.println(answer);
     }
 }
-/**
- * abcababcabc : abc
- * (abc)ab(abc)(abc) -> ab
- * 원래 길이 (11) -> replace 후 길이 (2) = 9개의 길이문자가 바뀜
- * 이는 즉 9개의 길이를 temp 로 나누면 바꾼 단어의 개수를 알 수 있다.
- */
+
