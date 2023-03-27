32439155ef624dfa64a36f7ba710b50737a9c98d
JaeJinByun <byunjin11@naver.com>
2023-03-27 23:51:15 +0900

    :triangular_flag_on_post: S3 [수이어쓰기] 구현, 문자열

---
+++ BaekJoon/src/silver/S3/S3_1515_수이어쓰기/Main.java
@@ -0,0 +1,33 @@
+package BaekJoon.src.silver.S3.S3_1515_수이어쓰기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * BaekJoon
+ * https://www.acmicpc.net/problem/1515
+ * Problem: 수이어쓰기
+ * Level: S3
+ * Algorithm: 구현, 문자열
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        int pt = 0;
+
+        int base = 0;
+        while (base++ <= 30000) {
+            String tmp = String.valueOf(base);
+            for (int i = 0; i < tmp.length(); i++) {
+                if (tmp.charAt(i) == s.charAt(pt))
+                    pt++;
+                if (pt == s.length()) {
+                    System.out.println(base);
+                    return;
+                }
+            }
+        }
+    }
+}
