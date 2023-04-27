c4c87115f9b9cfa7cbaaad31170ffa57402a8eee
JaeJinByun <byunjin11@naver.com>
2023-04-27 10:38:55 +0900

    [S4][비밀번호 찾기] 자료구조

---
+++ BaekJoon/src/silver/S4/S4_17219_비밀번호찾기/Main.java
@@ -0,0 +1,32 @@
+package BaekJoon.src.silver.S4.S4_17219_비밀번호찾기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        Map<String, String> map = new HashMap<>();
+        while (N-- > 0) {
+            st = new StringTokenizer(br.readLine(), " ");
+            map.put(st.nextToken(), st.nextToken());
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (M-- > 0) {
+            String key = br.readLine();
+            sb.append(map.get(key)).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
